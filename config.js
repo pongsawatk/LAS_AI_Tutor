992e0c7cfbee76fac4d35e6e433708c5c253f142
pongsawatk <pongsawatk@gmail.com>
2026-02-23 00:11:58 +0700

    Add log model

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 
 const CONFIG = {
     GEMINI_API_KEY: "AIzaSyB12a13LbEy-_uj-aUwlo3B03TsDC-jONk",
-    GOOGLE_SHEET_WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbxMTTaHFrZ5WG685zECDbUiN5BZDXTNx4-JbnjA6W8l0gG6YPH-KAaCpotPQaW680s5/exec"  // วาง URL ของ Google Apps Script Web App ที่นี่
+    GOOGLE_SHEET_WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbxu4HNRZF04XywxaxjqVNVcmeQB_Eem08v0Iq1lXh7DwN6YTV_UeyiYZZH9L1bCuchn/exec"  // วาง URL ของ Google Apps Script Web App ที่นี่
 };
